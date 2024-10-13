e85a024a7d0f19b04e254d007bf5238038b281ab
Charles Burks <c_s_burks@yahoo.com>
2024-10-13 11:37:07 -0700

    Update HMO script

--- script1_read_hmo_dat.R
+++ script1_read_hmo_dat.R
@@ -5,15 +5,14 @@ library(tidyverse)
 library(readxl)
 library(janitor)
 
-# Exmine files in path
-list.files()
+### Identify structure of spreadsheet, get the table into a data frame,
+### and adjust messy names and useless file formats
 
 # Read hmo data
-glimpse(read_excel("2025-hmo-non-postal-rates-fehb.xlsx"))
-
 hmo <- read_excel("2025-hmo-non-postal-rates-fehb.xlsx",
            range = "A3:O1445")
 
+# Make names manageable
 hmo <- clean_names(hmo)
 
 glimpse(hmo)
@@ -27,6 +26,7 @@ colnames(hmo)
 # [13] "x2025_monthly_govt_pays"               "x2025_monthly_empl_pays"              
 # [15] "x2025_monthly_change_in_empl_payment" 
 
+# Identify valuues for key variables
 unique(hmo$option)
 # [1] "Advantage"       "CDHP"            "Value"           "HDHP"            "High"           
 # [6] "Standard"        "Basic"           "Prosper"         "Saver"           "Blue Value Plus"
@@ -56,11 +56,40 @@ unique(hmo$location)
 unique(hmo$enrollment_type)
 # [1] "Self"          "Self & Family" "Self Plus One"
 
-hmo <- hmo %>% 
+# Make into a small managemeble data frame with appropriate data types
+hmo2 <- hmo %>% 
   select(c("plan","option","enrollment_code","location","enrollment_type","x2025_biweekly_empl_pays","x2025_biweekly_change_in_empl_payment")) %>% 
   rename(code = enrollment_code,
          type = enrollment_type,
          employee_biwkly = x2025_biweekly_empl_pays,
          change = x2025_biweekly_change_in_empl_payment) %>% 
   mutate(employee_biwkly = as.numeric(employee_biwkly),
-         change = as.numeric(employee_biwkly))
+         change = as.numeric(change))
+
+#### Pull out records of use to me
+cal_hmo <- hmo2 %>%  
+  filter(location == "California")           # Reduces from 1442 to 63 records
+
+unique(cal_hmo$plan)
+# [1] "Aetna Advantage"                            "Aetna Direct"                              
+# [3] "Aetna HealthFund CDHP and Aetna Value Plan" "Aetna HealthFund HDHP"                     
+# [5] "Aetna Open Access"                          "Anthem Blue Cross Select HMO"              
+# [7] "Health Net of California"                   "Kaiser Permanente - Fresno California"     
+# [9] "Kaiser Permanente - Northern California"    "Kaiser Permanente - Southern California"   
+# [11] "Sharp Health Plan"                          "Western Health Advantage"   
+
+cal_hmo %>% 
+  filter(str_detect(plan,"Aetna") | str_detect(plan,"Fresno"),
+         type == "Self & Family") %>% 
+  select(-c("location","type"))
+# # A tibble: 8 × 5
+#   plan                                       option    code  employee_biwkly change
+#   <chr>                                      <chr>     <chr>           <dbl>  <dbl>
+# 1 Aetna Advantage                            Advantage Z25              141. -11.8 
+# 2 Aetna Direct                               CDHP      N62              194.   6.67
+# 3 Aetna HealthFund CDHP and Aetna Value Plan Value     JS5              706. 119.  
+# 4 Aetna HealthFund CDHP and Aetna Value Plan CDHP      JS2              762.  31.3 
+# 5 Aetna HealthFund HDHP                      HDHP      225              241.  11.4 
+# 6 Aetna Open Access                          High      2X2              771. 100.  
+# 7 Kaiser Permanente - Fresno California      Standard  NZ5              199.  26.4 
+# 8 Kaiser Permanente - Fresno California      High      NZ2              341.  71.6 
